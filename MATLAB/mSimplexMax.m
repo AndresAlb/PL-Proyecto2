6663899970cfe18087a532c3816c6318f80eb5be
AndresAlb <aangele6@itam.mx>
2018-11-11 22:32:03 -0600

    Update MATLAB/mSimplexMax.m

--- MATLAB/mSimplexMax.m
+++ MATLAB/mSimplexMax.m
@@ -1,4 +1,4 @@
-function [x0, z0, ban, iter, sensinfo] = mSimplexMax(A, b, c, imprimirPasos) 
+function [x0, z0, ban, iter] = mSimplexMax(A, b, c, imprimirPasos) 
 
 % Esta funcion realiza la Fase II del Metodo Simplex para problemas
 % que tienen la siguiente forma
@@ -10,8 +10,10 @@ function [x0, z0, ban, iter, sensinfo] = mSimplexMax(A, b, c, imprimirPasos)
 %       b ... column vector with as many rows as A 
 %       c ... column vector with as many entries as one row of A 
 %       imprimirPasos ... boolean variable which indicates whether
-%       or not to print the step-by-step solution of the given problem 
-% 
+%       or not to print the step-by-step solution of the given problem. 
+%       Setting this parameter equal to true forces the program to use 
+%       a less efficient version of the Revised Simplex Method.
+%  
 % Out:  xo ..... SBF optima del problema 
 %       zo ..... valor optimo del problema 
 %       ban .... indica casos: 
@@ -20,18 +22,14 @@ function [x0, z0, ban, iter, sensinfo] = mSimplexMax(A, b, c, imprimirPasos)
 %           1 .... si la funcion objectivo no es acotada. 
 %       iter ... numero de iteraciones (cambios de variables basicas) 
 %       que hizo el metodo
-%       sensinfo ... Solo cuando ban = 0: 
-%           sensinfo.lambda ... es la solucion dual 
-%           sensinfo.gammas ... 2xn matrix con intervalos 
-%           sensinfo.betas ... 2xm matrix con intervalos
     
     format rat; % MATLAB imprime fracciones en vez de decimales
     
     iter = 0;
-
-    % 1 Definicion de las variables en estado 0 
-    
     c = -c;
+
+    % 1 Definicion de las variables iniciales
+    
     [m, n] = size(A); % m variables basicas
     ban = 0;
     [N, B, c, A] = deal( 1:n, (n+1):(m+n), [c' zeros(1,m)], [A eye(m)] );
@@ -40,7 +38,9 @@ function [x0, z0, ban, iter, sensinfo] = mSimplexMax(A, b, c, imprimirPasos)
     
     % Si el conjunto factible es vacio, el Metodo Simplex no
     % tendra opcion mas que escoger un punto que no cumpla
-    % la restriccion de no-negatividad.
+    % la restriccion de no-negatividad por lo que algun valor
+    % de b es negativo. En teoria, no deberiamos tener este 
+    % caso porque la documentacion pide b >= 0.
     if any(b < 0)
         
         if imprimirPasos
@@ -52,7 +52,7 @@ function [x0, z0, ban, iter, sensinfo] = mSimplexMax(A, b, c, imprimirPasos)
     end
     
     if imprimirPasos
-       imprimirTableau(A(:, N), c(B), rN, h, B, N, iter); 
+       imprimirTableau(A(:, B), A(:, N), c(B), rN, h, B, N, iter); 
     end
     
     % 2 Probamos la condicion de optimalidad
@@ -62,18 +62,20 @@ function [x0, z0, ban, iter, sensinfo] = mSimplexMax(A, b, c, imprimirPasos)
         % la Regla de Bland
         e = find(rN > 0, 1);
         
+        colEntrada = A(:, B)\A(:, N(e));
+        
         % 3 Probamos si el problema es acotado
-        if any(A(:, N(e)) > 0)
+        if any(colEntrada > 0)
             
             % 3.1 Seleccion de la variable de salida mediante
             % la Regla de Bland
 
             % Buscamos los indices de los denominadores no-positivos
-            noPositivos = A(:, N(e)) <= 0; 
+            noPositivos = colEntrada <= 0; 
 
             % Calculamos los cocientes y asignamos infinito a los
             % cocientes que tienen denominador menor o igual a cero 
-            cocientes = h./A(:, N(e));
+            cocientes = h./colEntrada;
             cocientes(noPositivos) = inf;
 
             % s es el indice que corresponde al minimo de cocientes
@@ -86,17 +88,16 @@ function [x0, z0, ban, iter, sensinfo] = mSimplexMax(A, b, c, imprimirPasos)
             
             % 4 Redefinimos los conjuntos B y N 
             [B(s), N(e)] = deal( N(e), B(s) );
-            [A(:, B), A(:, N), h] = deal( eye(m),...
-                A(:, B)\A(:, N), A(:, B)\h );
             iter = iter + 1;
             
             % 4.1 Calculamos los nuevos costos relativos 
             lambda = A(:, B)'\c(B)';
             lambda = lambda';
             rN = lambda*A(:, N) - c(N);
+            h = A(:, B)\b;
             
             if imprimirPasos
-                imprimirTableau(A(:, N), c(B), rN, h, B, N, iter);
+                imprimirTableau(A(:, B), A(:, N), c(B), rN, h, B, N, iter);
             end
             
         else
@@ -113,25 +114,20 @@ function [x0, z0, ban, iter, sensinfo] = mSimplexMax(A, b, c, imprimirPasos)
     
     if ban == 0
         x0 = zeros(m+n, 1);
-        x0(B) = A(:,B)\h;
+        x0(B) = h;
         z0 = -c*x0;
         
         % Solo devolvemos los valores de las variables originales
         x0 = x0(1:n); 
-        
-        % Analisis de sensibilidad
-        sensinfo.lambda = lambda;
-        sensinfo.gammas = [];
-        sensinfo.betas = [];
     else
-        [x0, z0, sensinfo] = deal( [], [], [] );
+        [x0, z0] = deal( [], [] );
     end
     
     return;
   
 end
 
-function imprimirTableau(AN, cB, rN, h, B, N, iter)
+function imprimirTableau(AB, AN, cB, rN, h, B, N, iter)
 
 % Este metodo imprime el tableau asociado a la base B
 % usando las matrices AB, AN y los vectores rN y h
@@ -142,7 +138,7 @@ function imprimirTableau(AN, cB, rN, h, B, N, iter)
     T = zeros(m+1,m+n+1);
 
     T(1:m, B) = eye(m);
-    T(1:m, N) = AN;
+    T(1:m, N) = AB\AN;
     T(1:m, m+n+1) = h;
     T(m+1, N) = rN;
     T(m+1, m+n+1) = cB*h;
