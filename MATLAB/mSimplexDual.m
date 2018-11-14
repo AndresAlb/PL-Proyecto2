8e7c2a2bfeba7e24e9201243206289755347448a
AndresAlb <aangele6@itam.mx>
2018-11-14 14:14:51 -0600

    Funcion mSimplexDual
    Probada con un ejercicio de clase

--- MATLAB/mSimplexDual.m
+++ MATLAB/mSimplexDual.m
@@ -1,4 +1,4 @@
-function [xo, zo, ban, iter, lambda0] = mSimplexDual(A, b, c) 
+function [x0, z0, ban, iter, lambda0] = mSimplexDual(A, b, c, imprimirPasos) 
 
 % Esta funcion realiza el Metodo Simplex Dual para problemas primales
 % que tienen la siguiente forma:
@@ -19,6 +19,138 @@ function [xo, zo, ban, iter, lambda0] = mSimplexDual(A, b, c)
 %       iter ... numero de iteraciones que hizo el metodo 
 %       lambda0 ... Solucion del problema dual 
 
+    format rat; % MATLAB imprime fracciones en vez de decimales
+    
+    iter = 0;
+    [x0, z0, lambda0] = deal( [], [], [] );
 
+    % 1 Definicion de las variables iniciales
+    
+    [m, n] = size(A); % m variables basicas
+    ban = 0;
+    [N, B, c, A] = deal( 1:n, (n+1):(m+n), [c' zeros(1,m)], [-A eye(m)] );
+    [lambda, h] = deal( c(B), -b );
+    rN = lambda*A(:, N) - c(N); 
+    Id = eye(m); % matriz identidad
+    
+    % Revisamos si el conjunto factible del problema dual
+    % es vacio
+    if any(c < 0)
+        
+        % Si el conjunto factible  del problema es vacio, 
+        % el Metodo Simplex dual no tendra opcion mas que escoger 
+        % un punto que no cumpla la restriccion de no-negatividad 
+        % por lo que algun valor de c es negativo. En teoria, no 
+        % deberiamos tener este caso porque la documentacion 
+        % pide c >= 0.
+        
+        if imprimirPasos
+            fprintf("\n\nConjunto factible vacio\n");
+        end
+        
+        ban = -1;
+        
+    end
+    
+    if imprimirPasos
+       imprimirTableau(A(:, B), A(:, N), c(B), rN, h, B, N, iter); 
+    end
+    
+    % 2 Probamos la condicion de optimalidad
+    while any(h < 0) && ban == 0
+        
+        % 2.1 Seleccionamos la variable de salida mediante
+        % la Regla de Bland
+        
+        s = find(h < 0, 1);
+        
+        filaSalida = (Id(s, :)/A(:, B))*A(:, N);
+        
+        % 3 Probamos si el problema es acotado
+        if any(filaSalida < 0)
+            
+            % 3.1 Seleccion de la variable de entrada mediante
+            % la Regla de Bland
+            
+            % Buscamos los indices de los denominadores no-negativos
+            noNegativos = filaSalida >= 0; 
+
+            % Calculamos los cocientes y asignamos infinito a los
+            % cocientes que tienen denominador mayor o igual a cero 
+            cocientes = rN./filaSalida;
+            cocientes(noNegativos) = inf;
+
+            % s es el indice que corresponde al minimo de cocientes
+            [~,e] = min(cocientes);
+            
+            if imprimirPasos
+                fprintf(['La variable de entrada es X%d y la '... 
+                    'variable de salida es X%d\n'],[N(e), B(s)]);
+            end
+            
+            % 4 Redefinimos los conjuntos B y N 
+            [B(s), N(e)] = deal( N(e), B(s) );
+            iter = iter + 1;
+            
+            % 4.1 Calculamos los nuevos costos relativos 
+            lambda = A(:, B)'\c(B)';
+            lambda = lambda';
+            rN = lambda*A(:, N) - c(N);
+            h = -A(:, B)\b;
+            
+            if imprimirPasos
+                imprimirTableau(A(:, B), A(:, N), c(B), rN, h, B, N, iter);
+            end
+            
+        else
+            
+            % El problema es no-acotado. 
+            ban = 1;
+            if imprimirPasos
+                fprintf("\n\nProblema no-acotado\n");
+            end
+        
+        end
+        
+    end
+    
+    if ban == 0
+        
+        x0 = zeros(m+n, 1);
+        x0(B) = h;
+        z0 = c*x0;
+        
+        % Solo devolvemos los valores de las variables originales
+        x0 = x0(1:n);
+        lambda0 = lambda;
+        
+    end
+    
+    return;
+  
+end
+
+function imprimirTableau(AB, AN, cB, rN, h, B, N, iter)
+
+% Este metodo imprime el tableau asociado a la base B
+% usando las matrices AB, AN y los vectores rN y h
+
+    m = length(B);
+    n = length(N);
+
+    T = zeros(m+1,m+n+1);
+
+    T(1:m, B) = eye(m);
+    T(1:m, N) = AB\AN;
+    T(1:m, m+n+1) = h;
+    T(m+1, N) = rN;
+    T(m+1, m+n+1) = cB*h;
+    
+    fprintf("\nVariables basicas:    ");
+    disp(B);
+    fprintf("Variables no-basicas: ");
+    disp(N);
+    fprintf("\nIteracion %d\n\n", iter);
+    disp(T);
 
 end
